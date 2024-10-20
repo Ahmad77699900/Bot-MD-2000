bd0c65cdc0a7ccd68c28730ca48f2b67ef9bde48
Ahmad77699900 <bbwth880@gmail.com>
2024-10-20 14:26:13 +0200

    Add files via upload
    Signed-off-by: Ahmad77699900 <bbwth880@gmail.com>

---
+++ plugins/رد2.js
@@ -0,0 +1,13 @@
+let handler = async (m, { conn }) => {
+    let user = global.db.data.users[m.sender];
+    let name = conn.getName(m.sender);
+    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
+    let message = '*مــنــور يـا قــلــب رزان🌚💔: لــي الـاوامــر اكــتــب (.اوامر) ⚡️*';
+await conn.sendMessage(m.chat, { react: { text: '⚡️', key: m.key } })
+    conn.sendFile(m.chat, 'https://envs.sh/06H.jpg', 'image.jpg', message, m);
+};
+
+handler.customPrefix = /^بوت$/i;
+handler.command = new RegExp;
+
+export default handler;
---
+++ plugins/رد3.js
@@ -0,0 +1,13 @@
+let handler = async (m, { conn }) => {
+    let user = global.db.data.users[m.sender];
+    let name = conn.getName(m.sender);
+    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
+    let message = `*قــلــب رزان مـن جـوا 👀⚡*`;
+await conn.sendMessage(m.chat, { react: { text: '🙌', key: m.key } })
+    conn.sendFile(m.chat, 'https://qu.ax/rNkWf.jpg', 'image.jpg', message, m);
+};
+
+handler.customPrefix = /^رزان$/i;
+handler.command = new RegExp;
+
+export default handler;
